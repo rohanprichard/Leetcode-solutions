ed588730d7f1817fd1531f4edb8dc3f55bb9dc12
rohanprichard <62638352+rohanprichard@users.noreply.github.com>
2022-05-15 21:10:07 +0530

    Update fizz-buzz.py

--- fizz-buzz.py
+++ fizz-buzz.py
@@ -1,3 +1,18 @@
+"""
+Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["1","2","Fizz"]
+"""
+
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         arr = [''] * n
