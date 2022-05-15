ecb61fe90273f3805fc08c8ae11fadf33095f931
rohanprichard <62638352+rohanprichard@users.noreply.github.com>
2022-05-15 21:05:53 +0530

    Update first-bad-version.py

--- first-bad-version.py
+++ first-bad-version.py
@@ -1,3 +1,11 @@
+"""
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+"""
+
 global b
 def isBadVersion(x):
     if x < b:
@@ -8,7 +16,6 @@ def firstBadVersion(n: int) -> int:
         l,r = 1, n
         while l < r:
             piv = int(r + l / 2)
-            print(piv,l,r)
 
             if isBadVersion(piv) == True and isBadVersion(piv-1) == False:
                 return piv
@@ -17,6 +24,7 @@ def firstBadVersion(n: int) -> int:
                 r = piv
             else:
                 l = piv + 1
+                
         return l
 
 b = 10
