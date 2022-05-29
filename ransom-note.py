b6915741c9766ce332f64bd72faec53297c33e70
rohanprichard <62638352+rohanprichard@users.noreply.github.com>
2022-05-30 02:11:56 +0530

    Add files via upload

--- ransom-note.py
+++ ransom-note.py
@@ -1,8 +1,26 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        for i in range(len(ransomNote)):
-            if ransomNote[i] in magazine:
-                magazine = magazine[:magazine.index(ransomNote[i])] + magazine[magazine.index(ransomNote[i]) + 1:]
-            else:
+        mag = {}
+        rans = {}
+        
+        for i in magazine:
+            try:
+                mag[i] += 1
+            except:
+                mag[i] = 1
+        for i in ransomNote:
+            try:
+                rans[i] += 1
+            except:
+                rans[i] = 1
+        
+        for n in rans:
+            try:
+                if mag[n] - rans[n] < 0:
+                    return False
+            except:
                 return False
+            
         return True
+                
+        
